25f2b20538203f052cb8925859f78c5748085485
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-24 09:53:05 +0900

    fix: client reference prepare destination on ssr

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,8 +1,14 @@
+import * as React from "react"
+
 export default function About() {
+  // verify this is client component
+  const [count, setCount] = React.useState(0);
+
   return (
     <main>
       <h1>About</h1>
       <p>This is the about page.</p>
+      <button onClick={() => setCount(c => c + 1)}>Count: {count}</button>
     </main>
   );
 }
--- packages/resolver/src/entry.ssr.tsx
+++ packages/resolver/src/entry.ssr.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react" assert { env: "react-client" };
 import { createRequestListener } from "@mjackson/node-fetch-server";
 import express from "express";
 // @ts-expect-error - no types
@@ -11,6 +11,7 @@ import {
 import { createFromReadableStream } from "react-server-dom-parcel/client.edge" assert { env: "react-client" };
 
 import { callServer } from "./entry.rsc.ts" assert { env: "react-server" };
+import type { ServerPayload } from "react-router/rsc";
 
 const app = express();
 
@@ -18,13 +19,24 @@ app.use("/client", express.static("dist/client"));
 
 app.use(
   createRequestListener(async (request) => {
+    // rsc decoding (flight client) needs to be triggered inside fizz context (ssr)
+    // so that `ReactDOM.preinit/preloadModule` (aka prepare destination) can hoist client reference scripts.
+    let payload: Promise<ServerPayload>;
+    function SsrRoot(props: { getPayload: () => Promise<ServerPayload> }) {
+      payload ??= props.getPayload();
+      return <RSCStaticRouter payload={React.use(payload) as any} />;
+    }
+
     return routeRSCServerRequest(
       request,
       callServer,
-      createFromReadableStream,
+      // hack to delay `decode`` inside `renderHTML`
+      // https://github.com/remix-run/react-router/blob/692ce42b6c7d1e2d7ddc43213585154fc1dfeabc/packages/react-router/lib/rsc/server.ssr.tsx#L54-L65
+      // @ts-ignore
+      (body) => () => createFromReadableStream(body),
       async (payload) => {
         return await renderHTMLToReadableStream(
-          React.createElement(RSCStaticRouter, { payload }),
+          React.createElement(SsrRoot, { getPayload: payload as any }),
           {
             bootstrapScriptContent: (
               callServer as unknown as { bootstrapScript: string }
