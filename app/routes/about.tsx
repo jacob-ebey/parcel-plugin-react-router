30dfba27b00cc00148e181f50607989616fddc5e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-05-13 10:55:49 +1000

    update to latest react-router experimental

---
+++ app/routes/about.tsx
@@ -0,0 +1,10 @@
+import "./about.css";
+
+export default function About() {
+  return (
+    <main>
+      <h1 className="about__heading">About</h1>
+      <p>This is the about page.</p>
+    </main>
+  );
+}
