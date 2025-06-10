876ead513d0229f144e61a239b010c39b6fe6497
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-06-10 15:55:14 +1000

    fix no options usage

--- packages/resolver/src/entry.express.tsx
+++ packages/resolver/src/entry.express.tsx
@@ -2,7 +2,7 @@ import { createRequestListener } from "@mjackson/node-fetch-server";
 import express from "express";
 import reactRouterRequestHandler from "./entry.request-handler.ts";
 
-function createExpressApp({ distDir = "dist" }: { distDir?: string }) {
+function createExpressApp({ distDir = "dist" }: { distDir?: string } = {}) {
   const app = express();
 
   app.use("/client", express.static(`${distDir}/client`));
